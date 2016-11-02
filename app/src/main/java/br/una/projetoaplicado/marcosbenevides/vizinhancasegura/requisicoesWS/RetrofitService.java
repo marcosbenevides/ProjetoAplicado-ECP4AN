b4779d4e37772fa3bcf091a60aa7c20749bcfd92
Marcos Benevides <marcosbenevides@outlook.com.br>
2016-11-02 10:55:28 -0200

    Call para alertas

--- app/src/main/java/br/una/projetoaplicado/marcosbenevides/vizinhancasegura/requisicoesWS/RetrofitService.java
+++ app/src/main/java/br/una/projetoaplicado/marcosbenevides/vizinhancasegura/requisicoesWS/RetrofitService.java
@@ -1,17 +1,16 @@
 package br.una.projetoaplicado.marcosbenevides.vizinhancasegura.requisicoesWS;
 
+import java.util.Date;
 import java.util.Objects;
 
+import br.una.projetoaplicado.marcosbenevides.vizinhancasegura.classes.Alerta;
 import br.una.projetoaplicado.marcosbenevides.vizinhancasegura.classes.Usuario;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 /**
  * Created by Marcos Benevides on 22/10/2016.
@@ -25,5 +24,15 @@ public interface RetrofitService {
     @GET("login/{email}/{senha}")
     Call<Usuario> login(@Path("email") String email, @Path("senha") String senha);
 
-
+    @FormUrlEncoded
+    @POST("cadastrarAlerta")
+    Call<Alerta> cadastrarAlerta(@Field("email") String email,
+                                 @Field("logHora")Date data,
+                                 @Field("longitude") String longitude,
+                                 @Field("latitude") String latitude,
+                                 @Field("bairro") String bairro,
+                                 @Field("cidade") String cidade,
+                                 @Field("estado") String estado,
+                                 @Field("obs") String obs,
+                                 @Field("tipo") String tipo);
 }
